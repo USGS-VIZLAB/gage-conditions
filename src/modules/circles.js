fd2b00850058ee1bad5554f147b850f25c1042b6
Carr <lcarr@usgs.gov>
2018-06-21 15:54:07 -0500

    add some quick mouseovers

--- src/modules/circles.js
+++ src/modules/circles.js
@@ -5,8 +5,15 @@ function add_circles(dv_stats_data) {
     .enter()
     .append("circle")
       .attr("cx", function(d, i) { return i*10 + 20; })
-      .attr("cy", 100)
-      .attr("r", function(d) { return d.per*10; });
+      .attr("cy", function(d, i) { return Math.random()*200; })
+      .attr("r", function(d) { return d.per*10; })
+      .on("mouseover", function(d) {
+        d3.select(this).attr("fill", "orange");
+        console.log(d.site_no);
+      })
+      .on("mouseout", function() {
+        d3.select(this).attr("fill", null);
+      });
 }
 
 export {add_circles};
